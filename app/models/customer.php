c2aa107a98ea8c956316a1e8bc6a22da2b519010
Redouane M. BOUDELLA <boudella.redouane@gmail.com>
2017-07-05 22:30:37 +0100

    added a statement function, responsible for returning a string evaluating the activity of the customer.

--- app/models/customer.php
+++ app/models/customer.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Exception;
 /**
  *
  */
@@ -28,11 +29,43 @@ class Customer
     function statement(){
         $totalAmount = 0;
         $frequesntRenterPoints = 0;
-        $result = "Rental Record for " . $this->getName() . "\n";
+        $result = "\nRental Record for " . $this->getName();
         foreach ($this->rentals as $index => $each) {
             $thisAmount = 0;
+            $movieType = $each->getMovie()->getType();
+            switch ($movieType) {
+                case Movie::CHILDRENS:
+                    $thisAmount += 1.5;
+                    if($each->getRentalDaysPeriod() > 3){
+                        $thisAmount += ($each->getRentalDaysPeriod() - 3)* 1.5;
+                    }
+                    break;
+                case Movie::NEW_RELEASE:
+                    $thisAmount += $each->getRentalDaysPeriod() * 3;
+                    break;
+                case Movie::REGULAR:
+                    $thisAmount += 2;
+                    if($each->getRentalDaysPeriod() > 2){
+                        $thisAmount += ($each->getRentalDaysPeriod() - 2) * 1.5;
+                    }
+                    break;
+                default:
+                    throw new Exception("Illegal Movie Type");
+                    break;
+            }
+
+            $frequesntRenterPoints++;
+
+            if($each->getMovie()->getType() == Movie::NEW_RELEASE
+                && $each->getRentalDaysPeriod() > 1){
+                $frequesntRenterPoints++;
+            }
+            $totalAmount += $thisAmount;
+            $result .= "\n- ".$each->getMovie()->getTitle()." : $thisAmount";
         }
 
+        $result .= "\nTotal spent : $totalAmount\n";
+        $result .= "\nYou earned : $frequesntRenterPoints frequent renter points\n";
         return $result;
     }
 }
--- tests/unit/customerTest.php
+++ tests/unit/customerTest.php
@@ -34,17 +34,64 @@ class CustomerTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    function can_append_statement_correctly(){
-        $rental_1 = $this->generateRental();
+    function can_append_statement_for_3_types(){
+        $rental_1 = $this->generateRental(Models\Movie::REGULAR,2);
+        $rental_2 = $this->generateRental(Models\Movie::CHILDRENS);
+        $rental_3 = $this->generateRental(Models\Movie::NEW_RELEASE,5);
+
         $customer = new Models\Customer("Redouane");
         $customer->addRental($rental_1);
+        $customer->addRental($rental_2);
+        $customer->addRental($rental_3);
+        $statement = $customer->statement();
 
-        $this->assertTrue(strlen($customer->statement()) > 0);
+        $expected = "\nRental Record for Redouane\n- Cool Movie : 2\n- Cool Movie : 1.5\n- Cool Movie : 15\nTotal spent : 18.5\n\nYou earned : 4 frequent renter points\n";
+        $this->assertEquals($expected, $statement);
     }
 
-    private function generateRental(){
-        $movie = new Models\Movie("Cool Movie", Models\Movie::NEW_RELEASE);
-        $rental = new Models\Rental($movie, 5);
+    /**
+     * @test
+     */
+    function can_append_statement_for_no_movie(){
+
+        $customer = new Models\Customer("Redouane");
+
+        $statement = $customer->statement();
+        $expected = "\nRental Record for Redouane\nTotal spent : 0\n\nYou earned : 0 frequent renter points\n";
+        $this->assertEquals($expected, $statement);
+    }
+
+    /**
+     * @test
+     */
+    function can_append_statement_for_1_movie_0_rental_period(){
+        $rental_1 = $this->generateRental(Models\Movie::REGULAR,0);
+        $customer = new Models\Customer("Redouane");
+        $customer->addRental($rental_1);
+        $statement = $customer->statement();
+        $expected = "\nRental Record for Redouane\n- Cool Movie : 2\nTotal spent : 2\n\nYou earned : 1 frequent renter points\n";
+        $this->assertEquals($expected, $statement);
+    }
+
+    /**
+     * @test
+     */
+    function can_append_statement_for_same_movie_multiples(){
+        $rental_1 = $this->generateRental(Models\Movie::REGULAR,3);
+        $rental_2 = $this->generateRental(Models\Movie::REGULAR,1);
+        $rental_3 = $this->generateRental(Models\Movie::REGULAR,5);
+        $customer = new Models\Customer("Redouane");
+        $customer->addRental($rental_1);
+        $customer->addRental($rental_2);
+        $customer->addRental($rental_3);
+        $statement = $customer->statement();
+        $expected = "\nRental Record for Redouane\n- Cool Movie : 3.5\n- Cool Movie : 2\n- Cool Movie : 6.5\nTotal spent : 12\n\nYou earned : 3 frequent renter points\n";
+        $this->assertEquals($expected, $statement);
+    }
+
+    private function generateRental($movieType = 0, $rentalPeriod = 1){
+        $movie = new Models\Movie("Cool Movie", $movieType);
+        $rental = new Models\Rental($movie, $rentalPeriod);
         return $rental;
     }
 }
