90f742d6dbdfd39ce0abbd462d598a2ed45bccfa
Redouane M. BOUDELLA <boudella.redouane@gmail.com>
2017-07-05 16:56:53 +0100

    Added a class Movie with basic test for constructor

--- app/models/movie.php
+++ app/models/movie.php
@@ -1,11 +1,26 @@
 <?php
-namespace \App\Models;
+namespace App\Models;
 /**
  *
  */
-class ClassName extends AnotherClass
+class Movie
 {
-    
+    const NEW_RELEASE = 1, REGULAR = 0, CHILDRENS = 2;
+    protected $title;
+    protected $type;
+
+    public function __construct($title=null,$type=0){
+        $this->title = $title;
+        $this->type = $type;
+    }
+
+    function getTitle(){
+        return $this->title;
+    }
+
+    function getType(){
+        return $this->type;
+    }
 }
 
 
--- tests/unit/movieTest.php
+++ tests/unit/movieTest.php
@@ -1,12 +1,25 @@
 <?php
-
+use \App\Models\Movie;
 /**
  *
  */
-class SampleTest extends PHPUnit_Framework_TestCase
+class MovieTest extends PHPUnit_Framework_TestCase
 {
-    function testTrueAssertsToTrue(){
-        $this->assertTrue(true);
+    /**
+     * @test
+     */
+    function can_create_movie_instance(){
+        $movie = new Movie();
+        $this->assertNotNull($movie);
+    }
+
+    /**
+     * @test
+     */
+    function can_create_movie_instance_with_title_and_type(){
+        $movie = new Movie("Cool movie",Movie::NEW_RELEASE);
+        $this->assertEquals("Cool movie",$movie->getTitle());
+        $this->assertEquals(Movie::NEW_RELEASE,$movie->getType());
     }
 }
 
